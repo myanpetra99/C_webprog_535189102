1d561e91fc7261421c8e56ff1d8f1fd8fc94cf8d
Michael Yan Petra <535189102+MichaelYanPetra@users.noreply.github.com>
2020-10-01 08:51:09 +0700

    Added Praktikum Pertemuan 5

---
+++ Praktikum Pertemuan 5 JS DOM/aksesDOM/js/main.js
@@ -0,0 +1,23 @@
+function myUniversal() {
+    document.querySelector("*").style.backgroundColor = "green";
+  }
+
+  function myType() {
+    document.querySelector("h1").style.backgroundColor = "blue";
+  }
+  function myType() {
+    document.querySelector("h1").style.backgroundColor = "blue";
+  }
+  function myClass() {
+    document.querySelector(".container").style.backgroundColor = "red";
+  }
+  function myId() {
+    document.querySelector("#logo").style.backgroundColor = "blue";
+  }
+  function myAttribute() {
+    document.querySelector("a[target]").style.backgroundColor = "yellow";
+  }
+  function myGroup() {
+    document.querySelector("h2, h1").style.backgroundColor = "green";
+  }
+  
